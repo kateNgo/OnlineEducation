3ff16df0e938eab9b43308c66d9883e9a98e5247
phuong <ngo.kate@yahoo.com>
2018-11-28 20:04:18 +1100

    print iFrame

--- OnlineEducation/scripts/OnlineEducation.js
+++ OnlineEducation/scripts/OnlineEducation.js
@@ -3,3 +3,10 @@
     $("#ToPDFContent").attr("href", "/HelpOnline/Level3/ToPDFContent?id=" + str);
     //alert($("#SelectedLevel3Id").val());
 }
+function printiFrame() {
+
+    var frm = document.getElementById("the_iFrame").contentWindow;
+    frm.focus();// focus on contentWindow is needed on some ie versions
+    frm.print();
+    return false;
+}
